361af92cf62d674da07c7f914d7dbbc82a22b57f
Unknown <ibrahimhadzic45@gmail.com>
2018-08-15 18:11:40 +0200

    Idemo dalje

--- CardBringer2/CardBringer2/DbInteraction.cs
+++ CardBringer2/CardBringer2/DbInteraction.cs
@@ -9,26 +9,8 @@ namespace CardBringer2
 {
     class DbInteraction
     {
-        private const string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;
-              AttachDbFilename=C:\Users\Focho\Desktop\CardBringer2\BazaYgo.mdf;
-              Integrated Security=True;Connect Timeout=30";
-
+        private const string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\bro\Documents\r18030\CardBringer2\CardBringer2\BazaYgo.mdf;Integrated Security=True;Connect Timeout=30";
         public SqlConnection connection = new SqlConnection(connString);
-
-        //public void OpenConnection()
-        //{
-        //    connection.Open();
-        //}
-
-        //public void CloseConnection()
-        //{
-        //    connection.Close();
-        //}
-
-        //public void Select(string query)
-        //{
-            
-        //}
     }
 
     
